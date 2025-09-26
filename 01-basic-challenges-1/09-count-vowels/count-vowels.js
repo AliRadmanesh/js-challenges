3ea062a3c46b0c42c35b84af2b8b0b09308a9bf5
Ali Radmanesh <ali.radmanesh1995@gmail.com>
2025-09-26 10:55:49 +0330

    feat: Implement Count Vowels challenge

---
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -0,0 +1,15 @@
+function countVowels(str) {
+  let count = 0;
+  const vowels = ["a", "e", "i", "o", "u"];
+
+  str
+    .toLowerCase()
+    .split("")
+    .forEach((char) => {
+      if (vowels.includes(char)) count++;
+    });
+
+  return count;
+}
+
+module.exports = countVowels;
